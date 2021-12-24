ff6a2c86ce6bb52b615be9d6c1a0776daf1b29a3
Sagar-Sharma-7 <6969sagarsharma@gmail.com>
2021-12-24 11:47:22 +0530

    text content

---
+++ public/js/main.js
@@ -0,0 +1,10 @@
+score_div = document.querySelector(".score");
+banner = document.querySelector(".banner");
+info = document.querySelector(".info");
+restart = document.querySelector(".restart"); 
+boxes = document.querySelectorAll(".box");
+
+// text content
+score_div.textContent = "Reaction Game";
+info.textContent = "Click on the coloured box to start the time.";
+
