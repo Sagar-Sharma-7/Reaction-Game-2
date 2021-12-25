b76d103036b07d28dea480df14ab78cc69c34235
Sagar-Sharma-7 <6969sagarsharma@gmail.com>
2021-12-25 08:20:36 +0530

    first box default color

--- public/js/main.js
+++ public/js/main.js
@@ -6,5 +6,12 @@ boxes = document.querySelectorAll(".box");
 
 // text content
 score_div.textContent = "Reaction Game";
-info.textContent = "Click on the coloured box to start the time.";
+info.textContent = "Click on the Green box to start the time.";
 
+// coloured box
+first_box = Math.floor(Math.random() * 16);
+console.log(first_box);
+boxes[first_box].style.backgroundColor = `rgb(48, 250, 48)`;
+
+// colors
+const colors = [" #4C8BF5", "#FF781F"]
